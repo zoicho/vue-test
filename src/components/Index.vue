1ded58bde1cd0cd58dcc479ada5ee6b79a954298
zoicho <daveyrow@gmail.com>
2024-11-24 15:05:50 +0100

    update

---
+++ src/components/Index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import {RouterLink} from 'vue-router'
+</script>
+
+<template>
+  <div class="w-full h-full flex flex-row justify-center flex-wrap gap-8 p-20">
+    <RouterLink  to="/shuffle" className="p-4 bg-gray-200 rounded-md">
+      Shuffle Array items
+    </RouterLink>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
 import './css/index.css'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Index from './components/Index.vue'
+import ShuffleTest from './components/ShuffleTest.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: Index },
+  { path: '/shuffle', component: ShuffleTest },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+createApp(App)
+  .use(router)
+  .mount('#app')
